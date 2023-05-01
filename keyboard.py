dc94590061125bf9c23df4088352fa0f9cffe7c8
Evgenii Maslennikov <evgeny79@mail.ru>
2023-05-01 19:50:08 +0500

    Add keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,25 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+button_hi = KeyboardButton('Привет! 👋')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+
+greet_kb1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
+
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+
+inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
+inline_kb_full.add(InlineKeyboardButton('Вторая кнопка', callback_data='btn2'))
+inline_btn_3 = InlineKeyboardButton('кнопка 3', callback_data='btn3')
+inline_btn_4 = InlineKeyboardButton('кнопка 4', callback_data='btn4')
+inline_btn_5 = InlineKeyboardButton('кнопка 5', callback_data='btn5')
+inline_kb_full.add(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.insert(InlineKeyboardButton("query=''", switch_inline_query=''))
+inline_kb_full.insert(InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
+inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате", switch_inline_query_current_chat='wasd'))
+inline_kb_full.add(InlineKeyboardButton('Уроки aiogram', url='https://surik00.gitbooks.io/aiogram-lessons/content/'))
